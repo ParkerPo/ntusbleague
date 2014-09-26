9b420d56a92a5e0eddc24c042670bca8d17c6ff2
ParkerPo <b00705048@ntu.edu.tw>
2014-09-27 02:56:59 +0800

    setting

--- website/settings.py
+++ website/settings.py
@@ -132,13 +132,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
 
 STATIC_URL = '/static/'
 
-"""
-# Parse database configuration from $DATABASE_URL
-DATABASES['default'] =  dj_database_url.config()
-
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-# Allow all host headers
-ALLOWED_HOSTS = ['*']
-"""
